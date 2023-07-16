923109dd0e3d53ae7be87df06afec08218cb51bb
Arnaldo Ugarte <arnoldupdev@gmail.com>
2023-07-16 14:11:40 -0500

    Added some styling to <DetailScreen />

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,18 +1,76 @@
-import { Text, View } from "react-native"
+import { Dimensions, Image, ScrollView, StyleSheet, Text, View } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { RootStackParamList } from "../navigation/Navigation"
+import { forHorizontalIOS } from "@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators"
 
-interface Props extends 
-StackScreenProps<RootStackParamList, 'DetailScreen'> {};
+interface Props extends StackScreenProps<RootStackParamList, 'DetailScreen'> {};
+
+let screen = Dimensions.get('screen')
 
 export const DetailScreen = ({ route } : Props) => {
+
 	const movie = route.params.movie;
+	const uri = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
 	console.log(movie.title)
 
 	return (
-		<View>
-			<Text>DetailScreen</Text>
-			<Text>DetailScreenasdasd</Text>
-		</View>
+		<ScrollView>
+			<View style={styles.imageContainer}>
+				<Image 
+					source={{ uri }}
+					style={styles.posterImage}
+				/>
+			</View>
+
+			<View style={styles.marginContainer}>
+				<Text style={ styles.subtitle }>{ movie.original_title }</Text>
+				<Text style={ styles.title }>{ movie.title }</Text>
+			</View>
+		</ScrollView>
 	)
 }
+
+const styles = StyleSheet.create({
+	imageContainer: {
+		width: '100%',
+		height: (screen.height * 0.7),
+
+		overflow: 'hidden',
+		marginBottom: 25,
+
+		shadowColor: '#000',
+		// shadowColor: 'red',
+		shadowOffset: {
+			width: 0,
+			height: 10,
+		},
+		shadowOpacity: 0.24,
+		shadowRadius: 7,
+		
+		elevation: 9,
+		borderBottomEndRadius: 25,
+		borderBottomStartRadius: 25,
+
+
+		backgroundColor: 'red',
+		// borderRadius: 100,
+	},
+	posterImage: {
+		flex: 1,
+	},
+	marginContainer: {
+		marginHorizontal: 20,
+		marginTop: 20,
+	},
+	subtitle: {
+		color: 'black',
+		fontSize: 16,
+		opacity: 0.8
+	},
+	title: {
+		color: 'black',
+		fontSize: 20,
+		fontWeight: "bold",
+	}
+})
+
