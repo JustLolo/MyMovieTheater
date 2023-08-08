ca2de893ec5fbcf62ed6d22d9553840bbf95efe5
Arnaldo Ugarte <arnoldupdev@gmail.com>
2023-08-08 06:00:35 -0500

    tested this todo, 'transparent' doesn't look bad * it takes the default value of the system

--- client/src/helpers/getColores.tsx
+++ client/src/helpers/getColores.tsx
@@ -2,17 +2,14 @@ import ImageColors from "react-native-image-colors"
 import { handleError } from "../../../common/helpers";
 
 export const getImageColors = async ( uri:string ) => {
-	// TODO: handle error/promise
 	const [colors, error] = await handleError(ImageColors.getColors(uri, {}))
 	
 	let primary = 'transparent';
 	let secondary = 'transparent';
 
 	if (!colors) {
-		console.log('something is wrong')
 		return [primary, secondary]
 	}
-
 
 	if (colors.platform === 'android') {
 		primary = colors.dominant ?? primary;
